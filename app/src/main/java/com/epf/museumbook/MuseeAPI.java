01146287959432b83a51af8fd99029fa2eca9d19
kyuritaima <sulwen.delacroix@gmail.com>
2019-06-15 19:33:30 +0200

    Le post de photos marche

--- app/src/main/java/com/epf/museumbook/MuseeAPI.java
+++ app/src/main/java/com/epf/museumbook/MuseeAPI.java
@@ -7,8 +7,14 @@ import com.epf.museumbook.Modeles.Musee;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface MuseeAPI {
@@ -21,4 +27,9 @@ public interface MuseeAPI {
 
     @GET("musees/{id}/pictures")
     Call<ArrayList<String>> getImages(@Path("id") String id);
+
+    @Multipart
+    @POST("musees/{id}/pictures")
+    Call<String> uploadImage(@Part MultipartBody.Part file, @Path("id") String id);
+
 }
--- app/src/main/java/com/epf/museumbook/PictureActivity.java
+++ app/src/main/java/com/epf/museumbook/PictureActivity.java
@@ -2,12 +2,17 @@ package com.epf.museumbook;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.ContactsContract;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -21,11 +26,19 @@ import android.widget.Toast;
 import com.epf.museumbook.Modeles.DatabaseSQLiteHelper;
 import com.epf.museumbook.Modeles.Musee;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.OutputStream;
-import java.util.ArrayList;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PictureActivity extends AppCompatActivity {
     private Musee museum;
@@ -33,6 +46,7 @@ public class PictureActivity extends AppCompatActivity {
     private static final int REQUEST_CAMERA = 1;
     private ImageView pictureImg;
     private Bitmap postImage;
+    private String mCurrentPhotpPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,8 +85,15 @@ public class PictureActivity extends AppCompatActivity {
             //customerIv?.setImageBitmap(image)
             // Display the image
             pictureImg.setImageBitmap(postImage);
+            Uri image = getImageUri(this, postImage);
+            String filePath = getRealPathFromURIPath(image, context);
+            File file = new File(filePath);
 
-/*            //val path:String = Environment.getExternalStorageDirectory().toString()
+            RequestBody mFile = RequestBody.create(MediaType.parse("/*"), file);
+            MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), mFile);
+
+
+/*           //val path:String = Environment.getExternalStorageDirectory().toString()
             // Get the path to the external storage folder
             String path = Environment.getExternalStorageDirectory().toString();
 
@@ -84,29 +105,177 @@ public class PictureActivity extends AppCompatActivity {
             File file = new File(dir, getPictureName());
             //val baos = ByteArrayOutputStream()
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            image.compress(Bitmap.CompressFormat.PNG, 100, baos);    //Compress the image to a ByteArray Object
-            val photo = baos.toByteArray()
+            postImage.compress(Bitmap.CompressFormat.PNG, 100, baos);    //Compress the image to a ByteArray Object
+            byte[] photo = baos.toByteArray();
 
-            customer.profilePictureURL = photo
-            db.updateCustomer(customer) //store the image as ByteArray into the database
-            var out:OutputStream
+            //customer.profilePictureURL = photo
+            //db.updateCustomer(customer) //store the image as ByteArray into the database
+            OutputStream out;
             try {
-                out = FileOutputStream(file)
-                image.compress(Bitmap.CompressFormat.PNG,100,out)
-                out.flush()
-                out.close()
-            }catch (e:FileNotFoundException){
-                e.printStackTrace()
-                //No permission to access camera
-                val permissionRequest = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE) //get the permission from the user
-                requestPermissions(permissionRequest, REQUEST_CAMERA_PERMISSION)
+                out = new FileOutputStream(file);
+                postImage.compress(Bitmap.CompressFormat.PNG,100,out);
+                out.flush();
+                out.close();
+            }catch (FileNotFoundException e) {
+                System.out.println(e.getMessage() + "PictureActivity onActivityResult FileNotFoundException");
+            }catch (IOException e){
+                System.out.println(e.getMessage() + "PictureActivity onActivityResult IOException");
             }*/
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("http://vps449928.ovh.net/api/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            MuseeAPI museeAPI = retrofit.create(MuseeAPI.class);
+
+            Call<String> call = museeAPI.uploadImage(body, museum.getId());
+
+            call.enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+                    System.out.println("on Response in PictureActivity");
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    System.out.println("on Failuref in PictureActivity");
+                }
+            });
+
         }
+    }
+
+    private String getRealPathFromURIPath(Uri uri, Context context) {
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        /* DocumentProvider */
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    private Uri getImageUri(PictureActivity pictureActivity, Bitmap postImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        postImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), postImage, "Title", null);
+        return Uri.parse(path);
     }
 
     private String getPictureName() {
         DatabaseSQLiteHelper db = new DatabaseSQLiteHelper(context);
-        return "MuseumBookPicture" + museum.getNom() + Integer.toString(museum.getImagesUrl().size() - 1);
+        return "MuseumBookPicture" + museum.getId() + Integer.toString(museum.getImagesUrl().size() - 1);
     }
 
     @Override
@@ -127,5 +296,27 @@ public class PictureActivity extends AppCompatActivity {
     }
 
     private void startCamera() {
+    }
+
+
+    //create image name
+    private File createImageFile() throws IOException {
+        String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName="JPGE"+timeStamp+"_";
+        File storgeDir= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image=File.createTempFile(imageFileName,".jpg",storgeDir);
+        mCurrentPhotpPath="file:"+image.getAbsolutePath();
+        return image;
+    }
+
+
+    //save image
+    private void galleryAddpic(){
+        Intent mediaScanIntent=new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f=new File(mCurrentPhotpPath);
+        Uri contentUri=Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+
     }
 }
